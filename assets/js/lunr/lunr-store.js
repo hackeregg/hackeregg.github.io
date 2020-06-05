b803b8dacb13fa18c57f1be1d2fd472f0bbc9bb1
immortal <dip.patel.ict@gmail.com>
2020-06-05 23:18:14 +0530

    changed theme and init blog title

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Implementing a Simple Redirection Analytics with a touch of Data Science",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/06/03/Implementing-a-Simple-Redirection-Analytics.html",
+        "teaser": null
+      }]
