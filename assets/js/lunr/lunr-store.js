8e4ada96f0ef8fc9945059769aabc0d6f734dbf8
immortal <dip.patel.ict@gmail.com>
2020-06-06 11:55:45 +0530

    changed base url

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Implementing a Simple Redirection Analytics with a touch of Data Science",
         "excerpt":"Introduction We are aiming at building a simple redirection analytics system similar to that of bit.ly and shorturl using opensource toolset with further discussion on the scope of data science in it. Let us take an example of a simple food review website where users can review dishes, order food...","categories": ["python","analytics","flask","postgres","redirection"],
         "tags": [],
-        "url": "http://localhost:4000/python/analytics/flask/postgres/redirection/2020/06/03/Implementing-a-Simple-Redirection-Analytics.html",
+        "url": "https://hackeregg.github.io/python/analytics/flask/postgres/redirection/2020/06/03/Implementing-a-Simple-Redirection-Analytics.html",
         "teaser": null
       }]
